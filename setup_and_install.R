5326c34303c3e21aebf7522c232d841d5fb42664
Joshua Soon <mythicalwarrior.js@gmail.com>
2021-10-30 03:05:36 +0800

    comments for dependencies

--- setup_and_install.R
+++ setup_and_install.R
@@ -6,6 +6,8 @@ renv::install(c("tidyverse", "nnet", "ggplot2", "pheatmap", "RColorBrewer", "msi
                 "ggrepel", "installr", "pacman", "ggpubr", "rstatix", "devtools", "ggnewscale",
                 "ggvenn", "ggridges", "igraph"))
 
+# igraph needs Libgfortran4
+
 renv::install("reticulate")
 
 renv::install('Seurat')
@@ -25,11 +27,13 @@ renv::install("bioc::edgeR")
 renv::install("bioc::DEGreport")
 renv::install("bioc::EDASeq")
 renv::install("bioc::ComplexHeatmap")
+# sudo apt-get install libudunits2-dev libgdal-dev libgeos-dev libproj-dev libharfbuzz-dev libfribidi-dev
 renv::install("bioc::EnhancedVolcano")
 renv::install("bioc::ChIPseeker")
 renv::install("bioc::GenomicFeatures")
 renv::install("bioc::GenomicAlignments")
 renv::install("bioc::TCGAbiolinks")
+# sudo apt-get install -y libgsl-dev
 renv::install("bioc::TFBSTools")
 renv::install("bioc::universalmotif")
 
